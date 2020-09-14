c87853db5c6f1f4b52e70440cbcc652225591f7e
Dave Trimboli <davidatrimboli@gmail.com>
2020-09-14 12:21:22 -0400

    transactions in db.js

---
+++ public/db.js
@@ -0,0 +1,52 @@
+let db
+const request = indexedDB.open('todo', 1)
+
+request.onupgradeneeded = event => {
+  db = event.target.result
+  db.createObjectStore('pending', { autoIncrement: true })
+}
+
+request.onsuccess = event => {
+  db = event.target.result
+
+  if (navigator.onLine) {
+    checkDatabase()
+  }
+}
+
+request.onerror = event => {
+  console.log(event.target.errorCode)
+}
+
+const saveItem = item => {
+  const transaction = db.transaction(['pending'], 'readwrite')
+  const store = transaction.objectStore('pending')
+  store.add(item)
+}
+
+const checkDatabase = () => {
+  console.log('checking database')
+  const transaction = db.transaction(['pending'], 'readwrite')
+  const store = transaction.objectStore('pending')
+  const getAll = store.getAll()
+
+  getAll.onsuccess = () => {
+    if (getAll.result.length > 0) {
+      fetch('/api/transactions/bulk', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('user')}`
+        },
+        body: JSON.stringify(getAll.result)
+      })
+        .then(() => {
+          const transaction = db.transaction(['pending'], 'readwrite')
+          const store = transaction.objectStore('pending')
+          store.clear()
+        })
+    }
+  }
+}
+
+window.addEventListener('online', checkDatabase)
